3e0a911d0de9a77ec13ac8b8223e154278d62cbf
VladimirC <58824589+VladimirCojocaru@users.noreply.github.com>
2021-10-18 21:54:51 +0200

    Created Table Files

--- proiectfsd/src/App.tsx
+++ proiectfsd/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
 import Login from "./components/login";
+import Table from "./components/table";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Table/>
     </div>
   );
 }
