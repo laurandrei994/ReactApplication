18198ed12691e00c84ffa7cd08edfc34c20eac45
Laur Andrei <laurandrei994@gmail.com>
2021-10-20 14:21:20 +0300

    Added basic routing

--- proiectfsd/src/App.tsx
+++ proiectfsd/src/App.tsx
@@ -3,12 +3,41 @@ import './App.css';
 import Login from "./components/login";
 import Table from "./components/table";
 import Image from "./components/image";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/table">Table</Link>
+          </li>
+          <li>
+            <Link to="/image">Image</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/table">
+          <Table />
+        </Route>
+        <Route path="/image">
+         <Image/>
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
