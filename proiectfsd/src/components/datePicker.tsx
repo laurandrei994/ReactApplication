b7b4cf1beef8cdada259af6c3d16630b471cc130
Laur Andrei <laurandrei994@gmail.com>
2021-10-27 18:00:31 +0300

    Added some changes to datePicker.tsx and datePicker.scss

--- proiectfsd/src/components/datePicker.tsx
+++ proiectfsd/src/components/datePicker.tsx
@@ -1,77 +1,68 @@
 import '../scss/datePicker.scss'
-import React from 'react'
+import React, { Component } from 'react'
+import { TableHead, TableRow, TableCell, TableBody, TableContainer, Table } from '@material-ui/core';
 
-function DatePicker() {
-    return (  
-        <div className='Calendar'>
-            <div className="month">
-                <ul>
-                    <li>15&emsp;August&emsp;2018</li>
-                </ul>
+const CALENDAR = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+
+class DatePicker extends Component {
+    constructor(props: any) {
+        super(props);
+        this.state={
+            image: null
+        };
+    }
+
+    getHead = () => {
+        return (
+            <TableHead>
+                <TableRow>
+                    <TableCell>SU</TableCell>
+                    <TableCell>MO</TableCell>
+                    <TableCell>TU</TableCell>
+                    <TableCell>WE</TableCell>
+                    <TableCell>TH</TableCell>
+                    <TableCell>FR</TableCell>
+                    <TableCell>SA</TableCell>
+                </TableRow>
+            </TableHead>
+        );
+    }
+
+    getBody = () => {
+        const calendar = [];
+        const processedCalendar = CALENDAR.map(day => {
+            return <TableCell>{day}</TableCell>
+        });
+
+        for (let index = 0; index < processedCalendar.length / 7; index++){
+            calendar.push(<TableRow>{processedCalendar.slice(index*7, index*7 + 7)}</TableRow>)
+        }
+
+        return (
+            <TableBody>
+                {
+                    calendar
+                }
+            </TableBody>
+        );
+    }
+    render() {
+        const defaultStyle = {marginLeft: 500, marginTop: 25, width:"380px"};
+        return (
+            <div>
+                <h1 className="h1" style= {defaultStyle}> DatePicker </h1>
+                <h2 className="h2" style= {defaultStyle}> Aici ar trebui butoanele </h2>
+                <div className="Calendar" style={ defaultStyle}>
+                    <div className="weekdays">
+                        {this.getHead()}
+                    </div>
+                    <div className="days">
+                        {this.getBody()}
+                    </div>
+                </div>
             </div>
-
-            <ul className="weekdays">
-                <li>SU</li>
-                <li>MO</li>
-                <li>TU</li>
-                <li>WE</li>
-                <li>TH</li>
-                <li>FR</li>
-                <li>SA</li>
-            </ul>
-
-            <ul className="days">
-                <span id="other">
-                    <li>29</li>
-                    <li>30</li>
-                    <li>31</li>
-                </span>
-                <span id="current_month">
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
-                    <li>6</li>
-                    <li>7</li>
-                    <li>8</li>
-                    <li>9</li>
-                    <li>10</li>
-                    <li>11</li>
-                    <li>12</li>
-                    <li>13</li>
-                    <li>14</li>
-                    <li><span className="active">15</span></li>
-                    <li>16</li>
-                    <li>17</li>
-                    <li>18</li>
-                    <li>19</li>
-                    <li>20</li>
-                    <li>21</li>
-                    <li>22</li>
-                    <li>23</li>
-                    <li>24</li>
-                    <li>25</li>
-                    <li>26</li>
-                    <li>27</li>
-                    <li>28</li>
-                    <li>29</li>
-                    <li>30</li>
-                    <li>31</li>
-                </span>
-                <span id="other">
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
-                    <li>6</li>
-                    <li>7</li>
-                    <li>8</li>
-                </span>
-            </ul>
-        </div>
-    );
+        );
+    }
 }
 
 export default DatePicker;
